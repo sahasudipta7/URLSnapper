066507c2e37d0c78213a725591c1be8fa79318a1
Sudipta Saha <148531317+sahasudipta7@users.noreply.github.com>
2025-09-22 12:52:23 +0530

    design logo and banner

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -2,27 +2,40 @@ import React from 'react'
 import { Link,useNavigate } from 'react-router-dom'
 import { Button } from './ui/button'
 import { DropdownMenu,DropdownMenuContent,DropdownMenuTrigger,DropdownMenuLabel,DropdownMenuSeparator,DropdownMenuItem } from '@radix-ui/react-dropdown-menu'
+import { Avatar, AvatarFallback, AvatarImage } from '@radix-ui/react-avatar'
+import { LinkIcon, LogOut } from 'lucide-react'
 const Header = () => {
     const navigate = useNavigate()
     const user = true;
   return (
     <nav className='py-4 flex justify-between items-center'>
         <Link to="/">
-        <img src="/urlsnapperlogo.png" className='h-16' alt="Snapper Logo"/>
+        <img src="/URLSnapper_Black_White_New_Scissor.png" className='h-16' alt="Snapper Logo"/>
         </Link>
         <div>
             {!user ?
             <Button onClick={()=>navigate("/auth")}>Login</Button>
             :(
             <DropdownMenu>
-                <DropdownMenuTrigger>Open</DropdownMenuTrigger>
+                <DropdownMenuTrigger className='w-10 rounded-flow overflow-hidden'>
+                    <Avatar>
+                        <AvatarImage src="https://github.com/shadcn.png" className='h-10'/>
+                        <AvatarFallback>SS</AvatarFallback>
+                    </Avatar>
+                </DropdownMenuTrigger>
                 <DropdownMenuContent>
-                <DropdownMenuLabel>My Account</DropdownMenuLabel>
+                <DropdownMenuLabel>Sudipta Saha</DropdownMenuLabel>
                 <DropdownMenuSeparator />
-                <DropdownMenuItem>Profile</DropdownMenuItem>
-                <DropdownMenuItem>Billing</DropdownMenuItem>
-                <DropdownMenuItem>Team</DropdownMenuItem>
-                <DropdownMenuItem>Subscription</DropdownMenuItem>
+                <DropdownMenuItem className='flex items-center'>
+                    <LinkIcon className='mr-2 h-4 w-4'/>
+                    My Links
+                </DropdownMenuItem>
+                <DropdownMenuItem className='text-red-400 flex items-center'>
+                    <LogOut className='mr-2 h-4 w-4'/>
+                    <span>
+                        Logout
+                    </span>
+                </DropdownMenuItem>
                 </DropdownMenuContent>
             </DropdownMenu>
             )
--- src/pages/landing.jsx
+++ src/pages/landing.jsx
@@ -1,10 +1,20 @@
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
 import React from 'react'
 
 const LandingPage = () => {
   return (
     <div>
-      LandingPage
+      <h2 className='my-10 sm:my-16 sm:text-6xl lg:text-7xl text-center font-extrabold'>
+        Need a shorter link? We've got you covered.
+      </h2>
+      <form>
+        <Input/>
+        <Button>Trim!</Button>
+      </form>
+      <img src="/URLSnapper_Banner_Black_White.png" alt="urlsnapperbanner" className='w-full my-11'/>
     </div>
+    
   )
 }
 
